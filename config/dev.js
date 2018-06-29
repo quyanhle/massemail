1477322460d56279ecad29cd3d2f407af4ed885e
cba_alex <alex@cybridge.jp>
2018-06-29 19:23:57 +0900

    add frontend server react

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js
+module.exports = {
+	googleClientID: '311134057293-l9hglp1rbumjdcv0np9kfep4sfqst6kb.apps.googleusercontent.com',
+	googleClientSecret: '_ebjAbFly23fRIMfSjDfg-d5',
+	mongoURI: 'mongodb://admin:admin123@ds121331.mlab.com:21331/massemail-dev',
+	cookieKey: 'dsfasfadsfereagaafdafererijjeiqjr3dvaf'
+};
